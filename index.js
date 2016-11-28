186b8fd8611f20b80dfa84c4070b3d3d1c42d546
torben <t.sickert[~at~]gmail.com>
2016-11-28 04:16:32 +0100

    Automatic update to new version.

--- index.js
+++ index.js
@@ -393,207 +393,99 @@ export class GenericIsDefinedPipe implements PipeTransform {
         return !(object === undefined || nullIsUndefined && indicator === null)
     }
 }
-/* TODO
-.filter('genericLimitTo', ($window) -> (input, limit, begin) ->
-    ###
-        Implements native "String.substring()" method as filter and angular's
-        native "limitTo" filter in one.
-    ###
-    # TODO: Remove this method if angular-2.0 is ported.
-    if $window.Math.abs($window.Number limit) is $window.Infinity
-        limit = $window.Number limit
-    else
-        limit = $window.parseInt limit
-    return input if $window.isNaN limit
-    input = input.toString() if $window.angular.isNumber input
-    if not ($window.angular.isArray(input) or $window.angular.isString input)
-        return input
-    if not begin or $window.isNaN begin
-        begin = 0
-    else
-        begin = $window.parseInt begin
-    begin = $window.Math.max(0, input.length + begin) if begin < 0
-    if limit >= 0
-        return input.slice begin, begin + limit
-    else if begin is 0
-        return input.slice limit, input.length
-    input.slice $window.Math.max(0, begin + limit), begin
-).filter('genericShallowFilter', ($filter, $window) -> (
-    data, searchQuery, comparator=null
-) ->
-    ###Like angular's native filter but doesn't search recursively.###
-    if not $window.angular.isDefined(data) or not $window.angular.isDefined(
-        searchQuery
-    ) or searchQuery is ''
-        return data
-    match = (date, index, data) ->
-        if $window.angular.isFunction searchQuery
-            return searchQuery date, index, data
-        if $window.angular.isObject date
-            hasMatch = false
-            for key, value of date
-                if date.hasOwnProperty(key) and not $window.angular.isFunction(
-                    value
-                ) and key.charAt(0) isnt '$'
-                    if $window.angular.isObject searchQuery
-                        if searchQuery.hasOwnProperty key
-                            currentSearchQuery = "#{searchQuery[key]}"
-                        else
-                            continue
-                    else
-                        currentSearchQuery = "#{searchQuery}"
-                    value = "#{value}"
-                    if comparator isnt null and not comparator
-                        currentSearchQuery = currentSearchQuery.toLowerCase()
-                        value = value.toLowerCase()
-                    else if $window.angular.isFunction comparator
-                        if comparator value, currentSearchQuery
-                            hasMatch = true
-                        continue
-                    if comparator is true
-                        if $filter('genericEquals')(
-                            currentSearchQuery, value, null, 1
-                        )
-                            hasMatch = true
-                    else if value.indexOf(currentSearchQuery) isnt -1
-                        hasMatch = true
-            return hasMatch
-        searchQuery = "#{searchQuery}"
-        date = "#{date}"
-        if comparator isnt null and not comparator
-            searchQuery = searchQuery.toLowerCase()
-            date = date.toLowerCase()
-        else if $window.angular.isFunction comparator
-            return comparator date, searchQuery
-        return searchQuery is date if comparator is true
-        date.indexOf(searchQuery) isnt -1
-    result = []
-    for index, date of data
-        index = $window.parseInt index
-        result.push(date) if match date, index, data
-    result
-).filter('genericShallowFilterInverted', ($filter, genericTool) ->
-    ###Inverted version of the shallow filter implementation.###
-    # TODO test
-    genericTool.constructor.invertArrayFilter $filter 'genericShallowFilter'
-).filter('genericSliceObjects', ($window) -> (item) ->
-    ###Removes all model connections from given item.###
-    # TODO test
-    result = {}
-    for key, value of item
-        if not (['_', '$'].indexOf(
-            key.charAt 0
-        ) is -1 and $window.angular.isObject value)
-            result[key] = value
-    result
-).filter('genericSliceMethods', ($window) -> (item) ->
-    ###Removes all model connections from given item.###
-    # TODO test
-    result = {}
-    for key, value of item
-        if not (['_', '$'].indexOf(
-            key.charAt 0
-        ) is -1 and $window.angular.isFunction value)
-            result[key] = value
-    result
-).filter('genericFilterInverted', ($filter, genericTool) ->
-    ###Inverted version of angular's native filter implementation.###
-    genericTool.constructor.invertArrayFilter $filter 'filter'
-).filter('genericNullIgnoreFilterInverted', ($filter, genericTool) -> ->
-    ###
-        Inverted version of angular's native filter implementation which
-        doesn't filter if filter property is null.
-    ###
-    if arguments.length > 1 and arguments[1] is null
-        return arguments[0]
-    genericTool.constructor.invertArrayFilter($filter 'filter').apply this, arguments
-).filter('genericFilterIfDefined', ($filter, $window) -> (
-    data, filterName, indicator, parameter...
-) ->
-    ###
-        Apply given filter with given parameter if given indicator is defined.
-        If indicator isn't defined first argument will be returned.
-    ###
-    undefinedValues = [undefined, null]
-    if $window.angular.isArray filterName
-        undefinedValues = undefinedValues.concat filterName.slice(
-            0, filterName.length - 1)
-        filterName = filterName[filterName.length - 1]
-    if indicator not in undefinedValues
-        return $filter(filterName).apply this, [data, indicator].concat(
-            parameter)
-    data
-)
 // / endregion
 // region string
-*/
 @Pipe({name: 'genericStringReplace'})
 export class GenericStringReplacePipe implements PipeTransform {
     transform(
         string:string, search:string|RegExp, replacement:string = '',
-        modifier:string = ''
+        modifier:string = 'g'
     ):string {
         return string.replace(new RegExp(search, modifier), replacement)
     }
 }
-/*
-.filter('genericStringShowIfPatternMatches', ($window) -> (
-    string, pattern, invert
-) ->
-    ###Returns given string if it matches given pattern.###
-    indicator = new $window.RegExp(pattern, 'g').test string
-    indicator = not indicator if invert
-    if indicator then string else ''
-# TODO test
-).filter('genericStringStartsWith', ($window) -> (string, needle) ->
-    ###Replaces a string with given replacement.###
-    if string and typeof needle is 'string'
-        return string.startsWith(needle)
-    false
-# TODO test
-).filter('genericStringEndsWith', ($window) -> (string, needle) ->
-    ###Replaces a string with given replacement.###
-    if string and typeof needle is 'string'
-        return string.endsWith(needle)
-    false
-).filter('genericStringMatch', ($window) -> (pattern, subject) ->
-    ###Tests if given pattern matches against given subject.###
-    (new $window.RegExp(pattern)).test subject
-).filter('genericStringSliceMatch', ($window) -> (subject, pattern, index=0) ->
-    ###
-        Returns a matched part of given subject with given pattern. Default is
-        the whole (zero) matched part.
-    ###
-    return subject.match(new $window.RegExp pattern)[index] if subject
-    ''
-).filter('genericStringGetLastDataStateHeaderName', (
-    $filter, genericOption
-) -> (resourceName='data') ->
-    ###Determines last data write time stamp for given resource name.###
-    $filter('genericStringCapitalize')(
-        $filter('genericStringCamelCaseToDelimited')(
-            genericOption.lastDataWriteHeaderName
-        ).replace /-([a-z])/g, (match) -> "-#{match[1].toUpperCase()}"
-    ).replace 'Data', $filter('genericStringCapitalize') resourceName
-).filter('genericStringHasTimeSuffix', ($filter) -> (string) ->
-    ###Determines if given string has a time indicating suffix.###
-    return false if not string?
-    $filter('genericStringEndsWith')(string, 'DateTime') or $filter(
-        'genericStringEndsWith'
-    )(string, 'Date') or $filter('genericStringEndsWith')(
-        string, 'Time'
-    ) or string is 'timestamp'
-)
+/**
+ * Returns given string if it matches given pattern.
+ */
+@Pipe({name: 'genericStringShowIfPatternMatches'})
+export class GenericStringShowIfPatternMatechsPipe implements PipeTransform {
+    transform(
+        string:string, pattern:string, invert:boolean = false,
+        modifier:string = 'g'
+    ):string {
+        indicator = new $window.RegExp(pattern, modifier).test(string)
+        if (invert)
+            indicator = !indicator
+        return indicator ? string : ''
+    }
+}
+/**
+ * Replaces a string with given replacement.
+ */
+@Pipe({name: 'genericStringStartsWith'})
+export class GenericStringStartsWithPipe implements PipeTransform {
+    transform(string:?string, needle:?string):boolean {
+        return string && typeof needle === 'string' && string.startsWith(
+            needle)
+    }
+}
+/**
+ * Replaces a string with given replacement.
+ */
+@Pipe({name: 'genericStringEndsWith'})
+export class GenericStringEndsWith implements PipeTransform {
+    transform(string:?string, needle:?string):boolean {
+        return string && typeof needle === 'string' && string.endsWith(needle)
+    }
+}
+/**
+ * Tests if given pattern matches against given subject.
+ */
+@Pipe({name: 'genericStringMatch'})
+export class GenericStringMatchPipe implements PipeTransform {
+    transform(pattern:string, subject:string, modifier:string = ''):boolean {
+        return (new $window.RegExp(pattern, modifier)).test(subject)
+    }
+}
+/**
+ * Returns a matched part of given subject with given pattern. Default is the
+ * whole (zero) matched part.
+ */
+@Pipe({name: 'genericStringSliceMatch'})
+export class GenericStringSliceMatchPipe implements PipeTransform {
+    transform(
+        subject:?subject, pattern:string, index:number = 0,
+        modifier:string = ''
+    ):string {
+        return subject ? subject.match(new RegExp(
+            pattern, modifier
+        ))[index] : ''
+    }
+}
+/**
+ * Determines if given string has a time indicating suffix.
+ */
+@Pipe({name: 'genericStringHasTimeSuffix'})
+export class GenericStringHasTimeSuffix implements PipeTransform {
+    transform(string:?string):boolean {
+        if (typeof string !== 'string')
+            return false
+        return string.endsWith('DateTime') || string.endsWith(
+            'Date'
+        ) || string.endsWith('Time') || string === 'timestamp'
+    }
+}
 // / endregion
 // / region number
-.filter('genericNumberPercent', ($window) -> (part, all) ->
-    ###Returns part in percent of all.###
-    (part / all) * 100
-).filter('genericNumberDistance', ($filter) -> (number) ->
-    ###Returns a distance representation for given number.###
-    $filter('number')(number) + ' km'
-)
-*/
+/**
+ * Returns part in percent of all.
+ */
+@Pipe({name: 'genericNumberPercent'})
+export class GenericNumberPercent implements PipeTransform {
+    transform(part:number, all:number):number {
+        return (part / all) * 100
+    }
+}
 // / endregion
 // endregion
 // region components
@@ -796,6 +688,7 @@ export class GenericMediumInputComponent implements AfterViewInit {
     }
 }
 // endregion
+// region modules
 const declarations:Array<Object> = Object.keys(module.exports).filter((
     name:string
 ):boolean => name.endsWith('Component') || name.endsWith('Pipe')).map((
@@ -819,6 +712,7 @@ const modules:Array<Object> = [
     providers
 })
 export default class GenericModule {}
+// endregion
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
 // vim: foldmethod=marker foldmarker=region,endregion:
